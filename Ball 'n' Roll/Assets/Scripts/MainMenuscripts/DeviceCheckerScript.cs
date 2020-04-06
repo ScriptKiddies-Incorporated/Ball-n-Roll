4cd21d4cf1da219fb4c4ac868dfdf9298fbc112a
miroslav karagyozov <m_iro02@abv.bg>
2020-04-06 16:20:08 -0700

    Add device checker
    A new variable stores the information about the device type of the user and (creates two branches of the game further)- it will be added soon.

---
+++ Ball 'n' Roll/Assets/Scripts/MainMenuscripts/DeviceCheckerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceCheckerScript : MonoBehaviour
+{
+    public static bool devChecker = false; //if it's true the selected device is PC, but when it's false it is 
+                                           //                                                           Android.
+    void Start()
+    {
+            #if UNITY_STANDALONE_WIN
+        devChecker = true;
+        print(devChecker);
+                   #endif
+    }
+
+
+}
