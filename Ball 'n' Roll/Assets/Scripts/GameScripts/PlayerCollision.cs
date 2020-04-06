2351e92f6391620d28b4978c3d8d00d0ccff6c44
Relaxo143 <relaxo143@protonmail.com>
2020-04-06 22:00:16 +0300

    Add Green Checkpoint Sound Effect

--- Ball 'n' Roll/Assets/Scripts/GameScripts/PlayerCollision.cs
+++ Ball 'n' Roll/Assets/Scripts/GameScripts/PlayerCollision.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading;
 using System;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
     public static bool cameraPos = true;
+	public GameObject greenSound;
 
     private void OnCollisionEnter(Collision collisionInfo)
     {
@@ -16,9 +18,14 @@ public class PlayerCollision : MonoBehaviour
         {
             ObstacleSpawnerScript.timeController = false;
             Destroy(gameObject);
-             Thread.Sleep(500);
+             Thread.Sleep(350);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
+		if (collisionInfo.collider.name == "GreenCheckpoint(Clone)")
+		{
+			Instantiate(greenSound);
+		}
     }
 
 }
