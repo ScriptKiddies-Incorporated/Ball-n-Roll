09dbbaff4ce2f577f13507c3c9afc00c4e021924
Relaxo143 <relaxo143@protonmail.com>
2020-04-09 21:28:44 +0300

    Revert changes

--- Ball 'n' Roll/Assets/Scripts/GameScripts/PlayerCollision.cs
+++ Ball 'n' Roll/Assets/Scripts/GameScripts/PlayerCollision.cs
@@ -8,47 +8,64 @@ using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public static int exCounter = 0;
-    public static bool exBool = true;
-    public static GameObject particleSystem;
-    public Transform particleSystemPos;
-    public Transform ballPlayer;
-    public Vector3 particleSystemVec = new Vector3(0, 0, 0);
+	public static int exCounter = 0;
+	public static bool exBool = true;
+	public GameObject particleSystem;
+	public Transform particleSystemPos;
+	public Transform ballPlayer;
+	public Vector3 particleSystemVec = new Vector3(0, 0, 0);
 
-    public static bool cameraPos = true;
+	public static bool cameraPos = true;
 	public GameObject greenSound;
-    public static bool redSoundCondition = true;
+	public static bool redSoundCondition = true;
 
-    public void OnCollisionEnter(Collision collisionInfo)
-    {
-        particleSystemPos.position = ballPlayer.position + particleSystemVec;
+	public static AsyncOperation tryAgainLoader;
 
-        if (collisionInfo.collider.name == "Obstacle" || collisionInfo.collider.name == "Obstacle(Clone)")
-        {
-            ObstacleSpawnerScript.timeController = false;
-            redSoundCondition = true;
+
+	private void Awake()
+	{
+		StartCoroutine(LoadTryAgainAsyncNow());
+		StartCoroutine(UnloadMainMenu());
+	}
+
+	public void OnCollisionEnter(Collision collisionInfo)
+	{
+		particleSystemPos.position = ballPlayer.position + particleSystemVec;
+
+		if (collisionInfo.collider.name == "Obstacle" || collisionInfo.collider.name == "Obstacle(Clone)")
+		{
+			ObstacleSpawnerScript.timeController = false;
+			redSoundCondition = true;
 			Destroy(gameObject);
-            Instantiate(particleSystem, particleSystemPos.position, Quaternion.identity);
-            // Thread.Sleep(2000);
-            //  exBool = false;
-            // if (exCounter == 100)
-            // {
-              exBool = false;
+			Instantiate(particleSystem, particleSystemPos.position, Quaternion.identity);
+			Thread.Sleep(2000);
+			//  exBool = false;
+			// if (exCounter == 100)
+			// {
+			//  exBool = false;
 
-            if(particleSystem.activeSelf == true)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            }
-
-           // }
-           }
+			// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			tryAgainLoader.allowSceneActivation = true;
+			// }
+		}
 
 		if (collisionInfo.collider.name == "GreenCheckpoint(Clone)")
 		{
 			Instantiate(greenSound);
 		}
-    }
+	}
 
+	IEnumerator LoadTryAgainAsyncNow()
+	{
+		yield return null;
+		tryAgainLoader = SceneManager.LoadSceneAsync("TryAgainMenu");
+		tryAgainLoader.allowSceneActivation = false;
+		yield return null;
+	}
 
+	IEnumerator UnloadMainMenu()
+	{
+		SceneManager.UnloadSceneAsync("MainMenu");
+		return null;
+	}
 }
-
