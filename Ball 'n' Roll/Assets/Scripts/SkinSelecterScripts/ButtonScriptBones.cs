d8626a56f989e86c0bacce2101c22ecb7a041a03
Bifrost19 <miroto1234@abv.bg>
2020-04-10 19:18:31 +0300

    Test commit

--- Ball 'n' Roll/Assets/Scripts/SkinSelecterScripts/ButtonScriptBones.cs
+++ Ball 'n' Roll/Assets/Scripts/SkinSelecterScripts/ButtonScriptBones.cs
@@ -12,7 +12,6 @@ public class ButtonScriptBones : MonoBehaviour
         ScoreText.cordChecker = 0;
         ScoreText.timeController2 = false;
         ButtonScriptHexagonCarbon.skinChoice = "Bones";
-		// SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		PlayButtonScript.gameLoader.allowSceneActivation = true;
 		SceneManager.LoadScene("Game"); // this runs if the user changes their skin during the game
 	}
