755c22ab329aeb7c06df18b64baaa70a9a10cfae
Bifrost19 <miroto1234@abv.bg>
2020-04-14 04:20:43 +0300

    Add 8 more exclusive skins

---
+++ Ball 'n' Roll/Assets/Scripts/2SkinSelector/ButtonScriptPokeball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScriptPokeball : MonoBehaviour
+{
+    public void ButtonscriptPokeball()
+    {
+        ScoreText.objCount = 0;
+        ScoreText.cordChecker = 0;
+        ScoreText.timeController2 = false;
+        ButtonScriptHexagonCarbon.skinChoice = "Pokeball";
+       // PlayButtonScript.gameLoader.allowSceneActivation = true;
+        SceneManager.LoadScene("Game"); // this runs if the user changes their skin during the game
+    }
+}
