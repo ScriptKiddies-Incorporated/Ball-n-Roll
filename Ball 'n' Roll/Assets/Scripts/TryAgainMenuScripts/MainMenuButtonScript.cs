d0a430cc4905a7a5aae71209309a5de4254c2143
Bifrost19 <miroto1234@abv.bg>
2020-09-07 18:04:31 +0300

    Add main menu button in the try again menu

---
+++ Ball 'n' Roll/Assets/Scripts/TryAgainMenuScripts/MainMenuButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        ScoreText.objCount = 0;
+        ScoreText.cordChecker = 0;
+        VelocityCheckerScript.timeCounter = 0;
+        VelocityCheckerScript.secTimeCounter = 0;
+        ScoreText.timeController2 = false;
+        PlayerCollision.exBool = true;
+        PlayerCollision.collisionBool = false;
+        MovingObstacleScript.moveForce = 0;
+        PlaneCollisionScript.exCollCheck = true;
+        BehaviourMovement.forwardForce = 400;
+        BoostScrpit.speedControll = 1.5f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
